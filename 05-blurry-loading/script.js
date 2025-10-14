d806321c4f81238b0bdd9bcda6d2895ca48b1c64
Augusta <auguchinenye@gmail.com>
2025-10-14 06:48:33 +0100

    Added the blurry loading project

---
+++ 05-blurry-loading/script.js
@@ -0,0 +1,24 @@
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+let blurValue;
+let opacityValue;
+
+let timerId = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(timerId);
+  }
+  console.log(load);
+  loadText.innerText = `${load}%`;
+
+  opacityValue = 1 - (load / 100);
+  blurValue = 30 - (0.3 * load);
+  
+  loadText.style.opacity = `${opacityValue}`;
+  bg.style.filter = `blur(${blurValue}px)`;
+}
